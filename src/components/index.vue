ea803b29adb2b283301e10c7925563bb3e01e4f2
jserk <393076251@qq.com>
2017-09-25 19:12:58 +0800

    This my web

---
+++ src/components/index.vue
@@ -0,0 +1,589 @@
+<template>
+  <div>
+    <div class="banner bgDarkBrown">
+      <div class="bannerScreen"></div>
+      <div class="bannerWords">
+        <div class="bannerTitle">言之命至</div>
+        <div class="bannerTitleS">Kevin</div>
+      </div>
+    </div>
+    <div class="nav bgDark">
+      <div class="navContent">
+        <router-link class="navPart" to='#'>首页</router-link>
+        <router-link class="navPart" to='#'>技术博客</router-link>
+        <router-link class="navPart" to='#'>言之命至</router-link>
+        <router-link class="navPart" to='#'>心情</router-link>
+        <router-link class="navPart" to='#'>记录</router-link>
+      </div>
+    </div>
+    <div class="lzs contentPart bgDarkBrown">
+      <div class="contentTitle">Yu Jae Seok</div>
+      <div class="contentPartC">
+        <div class="lzsWPart">
+          <div class="lzsWIcon"><img src="/static/img/index/lzsIcon1.svg" alt=""></div>
+          <div class="lzsWLine"></div>
+          <div class="lzsWContent">
+            没有做过的，有难度的，辛苦的，才能称之为挑战，只知道找捷径的，并不能被称之为挑战
+          </div>
+        </div>
+        <div class="lzsWPart">
+          <div class="lzsWIcon"><img src="/static/img/index/lzsIcon2.svg" alt=""></div>
+          <div class="lzsWLine"></div>
+          <div class="lzsWContent">
+            只要我不放弃我喜欢的东西，就无法兼得这两者，放弃我爱的一部分，是为了保护我爱的另一部分，其实理由很简单，毕竟不能全部拥有
+          </div>
+        </div>
+        <div class="lzsWPart">
+          <div class="lzsWIcon"><img src="/static/img/index/lzsIcon3.svg" alt=""></div>
+          <div class="lzsWLine"></div>
+          <div class="lzsWContent" style="position:relative;top:15px;">
+            言之命至，人随己愿
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="experience contentPart bgLightBrown">
+      <div class="contentTitle">
+        EXPERIENCE
+        <img src="/static/img/index/exTitle.svg" alt="">
+      </div>
+      <div class="contentPartC">
+        <div class="exContentL">
+          <div class="exContentLTitle">TimeLine</div>
+          <div class="exContentLBox">
+            <div class="exContentRBoxLine exContentRBoxLine1"></div>
+            <div class="exContentRBoxLine exContentRBoxLine2"></div>
+            <div class="exContentRBoxLine exContentRBoxLine3"></div>
+            <div class="exStage">
+              <div class="exStageTime">1993</div>
+              <div class="exStageCircle">
+                <div class="exStageCircleS"></div>
+              </div>
+              <div class="exStageName">Born</div>
+              <div class="exStageIcon"><img src="/static/img/index/ex1.svg" alt=""></div>
+            </div>
+            <div class="exStage">
+              <div class="exStageTime">2005</div>
+              <div class="exStageCircle">
+                <div class="exStageCircleS"></div>
+              </div>
+              <div class="exStageName">study</div>
+              <div class="exStageIcon" style="left:-5px;"><img src="/static/img/index/ex2.svg" alt=""></div>
+            </div>
+            <div class="exStage" style="margin-bottom:150px;">
+              <div class="exStageTime">2011</div>
+              <div class="exStageCircle">
+                <div class="exStageCircleS"></div>
+              </div>
+              <div class="exStageName">Find</div>
+              <div class="exStageIcon" style="left:2px;"><img src="/static/img/index/ex3.svg" alt=""></div>
+            </div>
+            <div class="exStage">
+              <div class="exStageTime">Futu</div>
+              <div class="exStageCircle">
+                <div class="exStageCircleS"></div>
+              </div>
+              <div class="exStageName">Improve</div>
+              <div class="exStageIcon" style="left:-11px;"><img src="/static/img/index/ex4.svg" alt=""></div>
+            </div>
+          </div>
+        </div>
+        <div class="exContentR">
+          <div class="exContentRPart">
+            <div>零阳镇</div>
+            <div>一完小</div>
+            <div>天真</div>
+          </div>
+          <div class="exContentRPart">
+            <div>辗转</div>
+            <div>乒乓</div>
+            <div>单纯</div>
+          </div>
+          <div class="exContentRPart" style="margin-bottom:139px;">
+            <div>一中</div>
+            <div>专心</div>
+            <div>青春期</div>
+            <div>高考</div>
+          </div>
+          <div class="exContentRPart">
+            <div>迷茫</div>
+            <div>自信</div>
+            <div>软件工程</div>
+            <div>前端</div>
+            <div>努力</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="record bgDarkBrown contentPart">
+      <div class="contentTitle">RECORDS</div>
+      <div class="contentPartC">
+        <div class="recordPart">
+          <div class="recordTime">
+            <div class="recordDay">12</div>
+            <div class="recordYearMonth">
+              dec
+              <br/> 2015
+            </div>
+            <div class="recordBorder"></div>
+          </div>
+          <div class="recordTitle">
+            recorder It is my recorder It is my recorder
+          </div>
+          <div class="readBtn btnDark">READ ME</div>
+        </div>
+        <div class="recordPart">
+          <div class="recordTime">
+            <div class="recordDay">12</div>
+            <div class="recordYearMonth">
+              dec
+              <br/> 2015
+            </div>
+            <div class="recordBorder"></div>
+          </div>
+          <div class="recordTitle">
+            recorder It is my recorder It is my recorder
+          </div>
+          <div class="readBtn btnDark">READ ME</div>
+        </div>
+        <div class="recordPart">
+          <div class="recordTime">
+            <div class="recordDay">12</div>
+            <div class="recordYearMonth">
+              dec
+              <br/> 2015
+            </div>
+            <div class="recordBorder"></div>
+          </div>
+          <div class="recordTitle">
+            recorder It is my recorder It is my recorder
+          </div>
+          <div class="readBtn btnDark">READ ME</div>
+        </div>
+        <div class="recordPart">
+          <div class="recordTime">
+            <div class="recordDay">12</div>
+            <div class="recordYearMonth">
+              dec
+              <br/> 2015
+            </div>
+            <div class="recordBorder"></div>
+          </div>
+          <div class="recordTitle">
+            recorder It is my recorder It is my recorder
+          </div>
+          <div class="readBtn btnDark">READ ME</div>
+        </div>
+        <div class="recordPart">
+          <div class="recordTime">
+            <div class="recordDay">12</div>
+            <div class="recordYearMonth">
+              dec
+              <br/> 2015
+            </div>
+            <div class="recordBorder"></div>
+          </div>
+          <div class="recordTitle">
+            recorder It is my recorder It is my recorder
+          </div>
+          <div class="readBtn btnDark">READ ME</div>
+        </div>
+        <div class="recordPart">
+          <div class="recordTime">
+            <div class="recordDay">12</div>
+            <div class="recordYearMonth">
+              dec
+              <br/> 2015
+            </div>
+            <div class="recordBorder"></div>
+          </div>
+          <div class="recordTitle">
+            recorder It is my recorder It is my recorder
+          </div>
+          <div class="readBtn btnDark">READ ME</div>
+        </div>
+      </div>
+    </div>
+    <div class="footer bgDark">
+      <div class="footerWebTitle">言之命至</div>
+      <div class="copyRight">Copyright © 2017 jser-K. 保留所有权利公安网备 陕ICP备16014833号-1</div>
+    </div>
+  </div>
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+@keyframes bannerBgAnimatin {
+  50% {
+    transform: translateY(-100px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+
+.banner {
+  width: 100%;
+  height: 410px;
+  position: relative;
+  overflow: hidden;
+  .bannerScreen {
+    -moz-transform: translate3d(0, 0, 0);
+    -ms-transform: translate3d(0, 0, 0);
+    -o-transform: translate3d(0, 0, 0);
+    transform: translate3d(0, 0, 0);
+    position: absolute;
+    width: 100%;
+    height: 200%;
+    background: url('/static/img/index/banner1.jpeg') no-repeat;
+    background-size: cover;
+    background-position: 0 0;
+    animation: bannerBgAnimatin 50s linear infinite; // filter: blur(1px);
+  }
+  .bannerWords {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    .bannerTitle {
+      width: 375px;
+      height: 91px;
+      line-height: 91px;
+      font-size: 58px;
+      font-weight: 100;
+      text-align: center;
+      margin-top: 79px;
+      margin-left: 55px;
+    }
+    .bannerTitleS {
+      width: 37px;
+      height: 22px;
+      line-height: 22px;
+      font-size: 16px;
+      font-weight: 100;
+      text-align: center;
+      margin-top: 29px;
+      margin-left: 137px;
+    }
+  }
+}
+
+.nav {
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  height: 59px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
+  .navContent {
+    width: 1000px;
+    display: -moz-box;
+    display: -webkit-box;
+    display: box;
+    margin-left: auto;
+    margin-right: auto;
+    .navPart {
+      display: block;
+      -webkit-box-flex: 1;
+      font-family: PingFangSC;
+      font-size: 15px;
+      text-align: center;
+      color: #9b9b9b;
+      text-decoration: none;
+      line-height: 59px;
+      transition: all 0.5s ease;
+      &:hover {
+        color: white;
+      }
+    }
+  }
+}
+
+.contentPart {
+  width: 100%;
+  height: auto;
+  overflow: hidden;
+  .contentTitle {
+    font-family: Helvetica;
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    font-size: 18px;
+    font-weight: 500;
+    margin-top: 10px;
+    img {
+      position: relative;
+      top: 7px;
+      left: -2px;
+      width: 27px;
+      height: 27px;
+    }
+  }
+  .contentPartC {
+    overflow: hidden;
+    width: 800px;
+    height: auto;
+    margin-top: 35px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+.lzs {
+  padding: 30px 0 30px 0;
+  .lzsWPart {
+    width: 100%;
+    height: 59px;
+    overflow: hidden;
+    margin-bottom: 30px;
+    .lzsWIcon {
+      float: left;
+      width: 49px;
+      height: 57px;
+      img {
+        float: left;
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .lzsWLine {
+      float: left;
+      width: 178px;
+      height: 2px;
+      background: #979797;
+      margin-top: 27px;
+      margin-left: 54px;
+    }
+    .lzsWContent {
+      float: right;
+      width: 388px;
+      height: 49px;
+      line-height: 25px;
+      font-size: 12px;
+      text-align: left;
+    }
+  }
+}
+
+.experience {
+  font-size: 16px;
+  font-weight: 400;
+  text-align: center;
+  padding: 30px 0 30px 0;
+  .exContentL {
+    float: left;
+    width: 235px;
+    height: auto;
+    overflow: hidden;
+    font-size: 13px;
+    .exContentLTitle {
+      width: 100%;
+      line-height: 22px;
+    }
+    .exContentLBox {
+      width: 100%;
+      padding-left: 20px;
+      margin-top: 10px;
+      position: relative;
+      .exContentRBoxLine {
+        position: absolute;
+        width: 2px;
+        background: $fD;
+        left: 82px;
+      }
+      .exContentRBoxLine1 {
+        top: 20px;
+        height: 32px;
+      }
+      .exContentRBoxLine2 {
+        top: 68px;
+        height: 32px;
+      }
+      .exContentRBoxLine3 {
+        top: 113px;
+        height: 165px;
+      }
+      .exStage {
+        width: 100%;
+        height: 27px;
+        line-height: 27px;
+        margin-bottom: 20px;
+        >div {
+          float: left;
+        }
+        .exStageTime {
+          width: 35px;
+          text-align: left;
+        }
+        .exStageCircle {
+          width: 12px;
+          height: 12px;
+          border-radius: 8px;
+          margin-left: 20px;
+          margin-top: 5px;
+          border: 2px solid #302929;
+          position: relative;
+
+          .exStageCircleS {
+            position: absolute;
+            width: 10px;
+            height: 10px;
+            border-radius: 5px;
+            left: 50%;
+            top: 50%;
+            margin-left: -5px;
+            margin-top: -5px;
+            background: $fD;
+          }
+        }
+
+        .exStageName {
+          margin-left: 20px;
+        }
+
+        .exStageIcon {
+          position: relative;
+          width: 38px;
+          height: 41px;
+          margin-left: 19px;
+          margin-top: -7px;
+          img {
+            width: 100%;
+            height: 100%;
+          }
+        }
+      }
+    }
+  }
+  .exContentR {
+    float: right;
+    margin-left: 100px;
+    height: 299px;
+    width: 440px;
+    margin-top: 21px;
+    font-size: 13px;
+    .exContentRPart {
+      width: 100%;
+      height: 41px;
+      box-sizing: border-box;
+      line-height: 39px;
+      border: 2px solid $fD;
+      margin-bottom: 7px;
+      display: flex;
+      >div {
+        flex: 1;
+      }
+    }
+  }
+}
+
+
+
+
+
+.record {
+  padding: 30px 0 30px 0;
+  min-height: 500px;
+  .recordPart {
+    width: 100%;
+    height: 53px;
+    padding-bottom: 10px;
+    margin-bottom: 30px;
+    border-bottom: 2px dashed #979797;
+    >div {
+      float: left;
+    }
+    .recordTime {
+      width: 120px;
+      height: 100%;
+      font-family: Impact;
+      color: $fD;
+      .recordDay {
+        float: left;
+        font-size: 45px;
+        text-align: center;
+      }
+      .recordYearMonth {
+        float: left;
+        width: 30px;
+        height: 36px;
+        font-size: 15px;
+        text-align: center;
+        margin-top: 9px;
+        margin-left: 5px;
+      }
+      .recordBorder {
+        float: right;
+        position: relative;
+        width: 3px;
+        height: 36px;
+        top: 9px;
+        background: rgba(79, 78, 78, 0.77);
+      }
+    }
+    .recordTitle {
+      float: left;
+      width: 460px;
+      height: 53px;
+      line-height: 53px;
+      margin-left: 43px;
+      text-align: left;
+      font-family: STSongti-SC;
+      font-size: 18px;
+      font-weight: 300;
+      color: $fD;
+      margin-left: 90px;
+    }
+    .readBtn {
+      float: right;
+      width: 102px;
+      height: 34px;
+      font-size: 16px;
+      line-height: 34px;
+      font-family: Impact;
+      text-align: center;
+      font-weight: 300;
+      margin-top: 10px;
+      color: $fL;
+    }
+  }
+}
+
+.footer {
+  width: 100%;
+  height: 100px;
+  overflow: hidden;
+  line-height: 29px;
+  color: white;
+  position: relative;
+  text-align: center;
+  .footerWebTitle {
+    width: 100%;
+    font-size: 15px;
+    margin-top: 20px;
+    height: 29px;
+  }
+  .copyRight {
+    width: 100%;
+    position: absolute;
+    bottom: 10px;
+    font-size: 12px;
+    bottom: 2px;
+  }
+}
+</style>
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your web'
+    }
+  }
+}
+</script>
+
+
